cad2ec305322b57125941d550938fbcf392f2a15
jmbg <jonah.berg.g@gmail.com>
2026-02-23 13:00:02 -0800

    chore: update OG image with latest UI screenshot, bump SW cache v7

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v6';
+const CACHE_VERSION = 'v7';
 const PAGE_CACHE = `blueboard-pages-${CACHE_VERSION}`;
 const DATA_CACHE = `blueboard-data-${CACHE_VERSION}`;
 const STATIC_CACHE = `blueboard-static-${CACHE_VERSION}`;
